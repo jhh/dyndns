8427467934f660ebffcc1f9d3258aff347052c71
Jeff Hutchison <jeff@jeffhutchison.com>
2021-06-25 14:15:09 -0400

    Migrate from Pipenv to Poetry

--- dyndns/settings.py
+++ dyndns/settings.py
@@ -1,9 +1,10 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 DYNDNS_HOST = os.environ["DYNDNS_HOST"]
 AWS_ACCESS_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET = os.environ["AWS_SECRET_ACCESS_KEY"]
 ZONE_ID = os.environ["AWS_ZONE_ID"]
 DNS_SERVER = os.environ["DNS_SERVER"]
 IPIFY_API_URI = "https://api.ipify.org"
-
